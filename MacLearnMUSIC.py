a91837c17db1d8856bf37ea089d1f489ce613ffe
Gregoire Pihan <g.pihan@gmail.com>
2024-09-01 22:57:37 -0400

    Add remaining work

--- MacLearnMUSIC.py
+++ MacLearnMUSIC.py
@@ -24,6 +24,26 @@ if __name__ == "__main__":
     ParamTrData.read_parameters(path_to_parameters_file, "data_parameters")
 
     
+    # Instantiate MacLearnProcessor
+    # Check if models need to be trained 
+    # if so, read the training data, (check formatting of the data) train the models
+    # save the trained models in TrainedModels
+    # Compute the initial conditions from initialConditions object
+    # use sym link to compute these directly into the output folder
+    # generate an emulator object for each trained model asked
+    # move emulator object as well as a copy of emulator class 
+    # in the output folder
+    # perform the prediction using the MLDriver in 3dMCGlauber
+    # (re adapt the code)
+    # ask why the shape of the input data does not use encoder anymore
+    # save the prediction into the same folder 
+    # profit
+
+    # Adapt the code for computation on the cluster 
+    # parallelize each predictions as much as possible
+    # parallelize centralities (or modelType BB, Bp, QQ ...) 
+    # with joblib or concurrent.futures
+    
 
 
 
--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Machine leaning for QCD hydrodynamic simulations using MUSIC.
 
 Use the option `--recurse-submodule` with `git clone` to fetch the 3dMCGlauber repository
+
+
+
+
+Remaining work
+
+- Initial conditions -> adapt for 3DMCGlauber
+- MacLearnProcessor (check need for training)
+- main
