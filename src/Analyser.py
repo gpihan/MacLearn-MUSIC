3a83fff3db71e7f438317a1de9e223e01c3ec38b
Grégoire <g.pihan@gmail.com>
2024-11-19 18:34:53 -0600

    Fix Analysis

--- src/Analyser.py
+++ src/Analyser.py
@@ -47,23 +47,8 @@ class Analyser():
             self.TrainTestDict = {}
 
     
-    def ComputeMidRap(self, Arr, y1=-0.5, y2=0.5):
-        i1 = np.argmin(np.abs(self.EtaOut - y1))
-        i2 = np.argmin(np.abs(self.EtaOut - y2))
-        midRapArr = np.mean(Arr[:,i1:i2], axis=1)
-        return midRapArr
-
-    def ComputeRMSArray(self, A):
-        return np.sqrt(np.sum(A**2, axis=0)/len(A[:,0]))
-
-    def ComputeRMS(self, X):
-        return np.sqrt(np.sum(X**2)/len(X))
-
     def ComputeIntegralArray(self, Arr):
-        return simpson(Arr, self.EtaOut, axis=1) 
-
-    def ComputeIntegral(self, Y):
-        return simpson(Y, self.EtaOut)
+        return simpson(Arr, x=self.EtaOut, axis=1) 
 
     def GetCentralityIndices(self, CENT):
         CentralityDict = defaultdict(list)
@@ -71,42 +56,68 @@ class Analyser():
             CentralityDict[centrality].append(i)
         return CentralityDict
 
-    def ComputeRMSDiff(self, Xtest, Ytest, Ypred, CTest):
-        D = {}
-        Y = Ytest-Ypred
-        D["X"] = self.EtaOut
-        D["Y"] = self.ComputeRMSArray(Y) 
-        return D 
 
-    def ComputeRMSDiffCentrality(self, Xtest, Ytest, Ypred, CTest):
+    def global_charge(self, Ypred, Ytest):
+        IntPred = self.ComputeIntegralArray(Ypred)
+        IntTest = self.ComputeIntegralArray(Ytest)
+        return np.array(IntPred) - np.array(IntTest)
+
+    def mean_absolute_error(self, Ypred, Ytest):
+        Ymean = np.mean(Ytest, axis=1)
+        return np.mean(np.abs(Ypred - Ytest), axis=1)/Ymean * 100
+
+    def root_mean_squared_error(self, Ypred, Ytest):
+        Ymean = np.mean(Ytest, axis=1)
+        mse = np.mean((Ypred - Ytest) ** 2, axis=1)
+        return np.sqrt(mse)/Ymean * 100
+
+    def r_squared(self, Ypred, Ytest):
+        y_mean = np.mean(Ytest, axis=1, keepdims=True)
+        ss_total = np.sum((Ytest - y_mean) ** 2, axis=1)
+        ss_residual = np.sum((Ytest - Ypred) ** 2, axis=1)
+        return 1 - (ss_residual / ss_total)
+
+    def correlation_coefficient(self, Ypred, Ytest):
+        means_test = np.mean(Ytest, axis=1, keepdims=True)
+        means_pred = np.mean(Ypred, axis=1, keepdims=True)
+        numerators = np.sum((Ytest - means_test) * (Ypred - means_pred), axis=1)
+        denominators = np.sqrt(np.sum((Ytest - means_test) ** 2, axis=1) *
+                               np.sum((Ypred - means_pred) ** 2, axis=1))
+        return numerators / denominators
+
+    def ComputeMAE(self, Xtest, Ytest, Ypred, CTest):
         IndexDict = self.GetCentralityIndices(CTest)
-        return {centr:{"X":self.EtaOut, "Y":self.ComputeRMSArray(Ytest[indexes] - Ypred[indexes])} for centr, indexes in IndexDict.items()}
+        return {centr:self.mean_absolute_error(Ypred[indexes], Ytest[indexes]) for centr, indexes in IndexDict.items()}
 
-    def ComputeMidRapidityRMSDiff(self, Xtest, Ytest, Ypred, CTest):
-        MidRapTest = self.ComputeMidRap(Ytest)
-        MidRapPred = self.ComputeMidRap(Ypred)
-        return self.ComputeRMS(MidRapTest - MidRapPred)
+    def ComputeRMSE(self, Xtest, Ytest, Ypred, CTest):
+        IndexDict = self.GetCentralityIndices(CTest)
+        return {centr:self.root_mean_squared_error(Ypred[indexes], Ytest[indexes]) for centr, indexes in IndexDict.items()}
 
-    def ComputeGlobalChargeDiff(self, Xtest, Ytest, Ypred, CTest):
-        IntegTest = self.computeIntegralArray(Ytest)
-        IntegPred = self.computeIntegralArray(Ypred)
-        return self.computeRMS(IntegTest - IntegPred)
+    def ComputeR2(self, Xtest, Ytest, Ypred, CTest):
+        IndexDict = self.GetCentralityIndices(CTest)
+        return {centr:self.r_squared(Ypred[indexes], Ytest[indexes]) for centr, indexes in IndexDict.items()}
+
+    def ComputeCorrelation(self, Xtest, Ytest, Ypred, CTest):
+        IndexDict = self.GetCentralityIndices(CTest)
+        return {centr:self.correlation_coefficient(Ypred[indexes], Ytest[indexes]) for centr, indexes in IndexDict.items()}
+
+    def ComputeGlobalCharge(self, Xtest, Ytest, Ypred, CTest):
+        IndexDict = self.GetCentralityIndices(CTest)
+        return {centr:self.global_charge(Ypred[indexes], Ytest[indexes]) for centr, indexes in IndexDict.items()}
 
     def GetRaw(self, Xtest, Ytest, Ypred, CTest):
         return {"Eta":self.EtaOut, "Xtest":Xtest, "Ytest":Ytest, "Ypred":Ypred, "Centralities":CTest}
 
 
-    def Full(self, Xtest, Ytest, Ypred, CTest):
-        return {
-                "ComputeRMSDiff":self.ComputeRMSDiff(Xtest, Ytest, Ypred, CTest), 
-                "ComputeMidRapidityRMSDiff":self.ComputeMidRapidityRMSDiff(Xtest, Ytest, Ypred, CTest), 
-                "ComputeGlobalChargeDiff":self.ComputeRMSDiff(Xtest, Ytest, Ypred, CTest), 
-                "ComputeRMSDiffCentrality":self.ComputeRMSDiffCentrality(Xtest, Ytest, Ypred, CTest),
-                "GetRaw":self.GetRaw(Xtest, Ytest, Ypred, CTest), 
-                }
 
-    def Light(self, Xtest, Ytest, Ypred, CTest):
-        return {"ComputeRMSDiff":self.ComputeRMSDiff(Xtest, Ytest, Ypred, CTest)}
+
+
+    def Full(self, Xtest, Ytest, Ypred, CTest):
+        return {"MAE":self.ComputeMAE(Xtest, Ytest, Ypred, CTest), 
+                "RMSE":self.ComputeRMSE(Xtest, Ytest, Ypred, CTest), 
+                "R2":self.ComputeR2(Xtest, Ytest, Ypred, CTest), 
+                "Corr":self.ComputeCorrelation(Xtest, Ytest, Ypred, CTest), 
+                "GC":self.ComputeGlobalCharge(Xtest, Ytest, Ypred, CTest)}
 
     def Raw(self, Xtest, Ytest, Ypred, CTest):
         return {"GetRaw":self.GetRaw(Xtest, Ytest, Ypred, CTest)}
